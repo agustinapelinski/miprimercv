1fca8a44f8b084dd95f5cb9358f32d0f7da2cf3b
agus <agustina.pelinski2502@gmail.com>
2023-01-28 15:39:35 -0300

    subiendo archivos

---
+++ function.js
@@ -0,0 +1,10 @@
+function CV(){
+    document.getElementById("boton").addEventListener("click", e=>{
+
+        window.location.href= "mailto:jeanette.johnson@example.com"
+        })
+        console.log ("mi primer cv en html");
+        
+        window.location.href ='mailto:jeanette.johnson@example.com'
+
+}
